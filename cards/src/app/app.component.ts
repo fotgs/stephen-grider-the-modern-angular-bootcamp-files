428147d0ac6158b5ed9b3a6212512cd2a39b993a
Annie Giang <agiang@nisum.com>
2020-01-20 11:57:46 -0800

    Store data in App

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "cards";
+
+  posts = [
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/tree.jpeg",
+      username: "nature",
+      content: "I saw this neat tree today."
+    },
+    {
+      title: "Snowy Mountain",
+      imageUrl: "assets/mountain.jpeg",
+      username: "mountainlover",
+      content: "Here is a picture of a snowy mountain."
+    },
+    {
+      title: "Mountain Biking",
+      imageUrl: "assets/biking.jpeg",
+      username: "biking1222",
+      content: "I did some biking today."
+    }
+  ];
 }
