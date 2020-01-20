d2c4e9cda826de59f8fbc904087759d87af043ba
Annie Giang <agiang@nisum.com>
2020-01-20 09:35:02 -0800

    Add event listeners and business logic on input

--- pw-generator/src/app/app.component.ts
+++ pw-generator/src/app/app.component.ts
@@ -6,9 +6,40 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  length = 0;
   password = "";
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
 
   onButtonClick() {
     this.password = "new password";
+    console.log(`
+    Include Letters: ${this.includeLetters};
+    Include Numbers: ${this.includeNumbers};
+    Include Symbols: ${this.includeSymbols};
+    `);
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    } else {
+      this.length = 0;
+    }
   }
 }
