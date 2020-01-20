899a287026a17fe18734cf290b001e24a3a87ac4
Annie Giang <agiang@nisum.com>
2020-01-20 13:39:49 -0800

    Display random sentence on load

--- typing-game/src/app/app.component.ts
+++ typing-game/src/app/app.component.ts
@@ -7,10 +7,21 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "typing-game";
+  sentences = [
+    "After the favorable episode",
+    "Any opera warps a straight menu",
+    "The arrested hobby kisses a soldier",
+    "The happy alcohol colors the mechanism."
+  ];
   userInput = "";
-  randomWord = "";
+  randomSentence = this.getRandomSentence();
 
   onChangeEvent(value: string) {
     this.userInput = value;
+  }
+
+  getRandomSentence() {
+    let randomIdx = Math.floor(Math.random() * this.sentences.length);
+    return this.sentences[randomIdx];
   }
 }
