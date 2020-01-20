7454607f43c9f6e63059e37b168315ef8f363e0e
Annie Giang <agiang@nisum.com>
2020-01-20 14:24:50 -0800

    Remove sentences

--- typing-game/src/app/app.component.ts
+++ typing-game/src/app/app.component.ts
@@ -7,16 +7,9 @@ import { lorem } from "faker";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "typing-game";
-  sentences = [
-    "After the favorable episode",
-    "Any opera warps a straight menu",
-    "The arrested hobby kisses a soldier",
-    "The happy alcohol colors the mechanism."
-  ];
   userInput = "";
-  randomSentence = lorem.sentence();
-  
+  randomText = lorem.sentence();
+
   onChangeEvent(value: string) {
     this.userInput = value;
   }
