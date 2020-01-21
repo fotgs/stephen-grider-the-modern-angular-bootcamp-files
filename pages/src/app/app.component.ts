4d28203b4fc95f180dea8505faa0d9d7d13cf752
Annie Giang <agiang@nisum.com>
2020-01-21 11:23:22 -0800

    Display title

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -9,22 +9,22 @@ export class AppComponent {
   currentPage = 0;
   images = [
     {
-      title: "At the beach",
+      title: "Beach",
       url:
         "https://images.unsplash.com/photo-1494879540385-bc170b0878a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
     },
     {
-      title: "At the park",
+      title: "Park",
       url:
         "https://images.unsplash.com/photo-1523849161192-3a6e717932c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1937&q=80"
     },
     {
-      title: "At the garden",
+      title: "Garden",
       url:
         "https://images.unsplash.com/photo-1548403374-728475ca2813?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80"
     },
     {
-      title: "At the cafe",
+      title: "Cafe",
       url:
         "https://images.unsplash.com/photo-1542181961-9590d0c79dab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
     }
