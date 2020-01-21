40da0eee539c71c418dba800756b1cc681b6011f
Annie Giang <agiang@nisum.com>
2020-01-21 11:27:58 -0800

    Add more data

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -27,6 +27,46 @@ export class AppComponent {
       title: "Cafe",
       url:
         "https://images.unsplash.com/photo-1542181961-9590d0c79dab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+    },
+    {
+      title: "Beach",
+      url:
+        "https://images.unsplash.com/photo-1494879540385-bc170b0878a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+    },
+    {
+      title: "Park",
+      url:
+        "https://images.unsplash.com/photo-1523849161192-3a6e717932c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1937&q=80"
+    },
+    {
+      title: "Garden",
+      url:
+        "https://images.unsplash.com/photo-1548403374-728475ca2813?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80"
+    },
+    {
+      title: "Cafe",
+      url:
+        "https://images.unsplash.com/photo-1542181961-9590d0c79dab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+    },
+    {
+      title: "Beach",
+      url:
+        "https://images.unsplash.com/photo-1494879540385-bc170b0878a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+    },
+    {
+      title: "Park",
+      url:
+        "https://images.unsplash.com/photo-1523849161192-3a6e717932c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1937&q=80"
+    },
+    {
+      title: "Garden",
+      url:
+        "https://images.unsplash.com/photo-1548403374-728475ca2813?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80"
+    },
+    {
+      title: "Cafe",
+      url:
+        "https://images.unsplash.com/photo-1542181961-9590d0c79dab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
     }
   ];
 }
