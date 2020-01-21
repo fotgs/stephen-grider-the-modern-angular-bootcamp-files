60d299d5acc1038daa515a73e2687f2faf754024
Annie Giang <agiang@nisum.com>
2020-01-21 10:08:21 -0800

    Create custom pipe to convert miles to km

---
+++ pipes/src/app/convert.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "convert"
+})
+export class ConvertPipe implements PipeTransform {
+  transform(value: any, ...args: any[]): any {
+    if (value) return value * 1.60934;
+  }
+}
