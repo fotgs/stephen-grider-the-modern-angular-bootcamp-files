352dae4e00cac270369b1460570c27019671232d
Annie Giang <agiang@nisum.com>
2020-01-21 10:54:06 -0800

    Refactor conditional logic to disable button

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  currentPage = 2;
+  currentPage = 3;
   images = [
     {
       title: "At the beach",
