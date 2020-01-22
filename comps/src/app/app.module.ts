30c06cb308678fe917c73f2fc1a688c39340b18d
Annie Giang <agiang@nisum.com>
2020-01-22 13:30:30 -0800

    Export ElementsHome and import Elements module

--- comps/src/app/app.module.ts
+++ comps/src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ElementsHomeComponent } from './element/elements-home/elements-home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ElementsModule } from "./elements/elements.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ElementsHomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ElementsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
