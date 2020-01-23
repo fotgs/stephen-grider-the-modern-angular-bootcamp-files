964ab5f307a83dda330b18bae624e76a5f510b47
Annie Giang <agiang@nisum.com>
2020-01-23 14:19:50 -0800

    Assemble table

--- comps/src/app/collections/collections-home/collections-home.component.ts
+++ comps/src/app/collections/collections-home/collections-home.component.ts
@@ -7,12 +7,13 @@ import { Component, OnInit } from "@angular/core";
 })
 export class CollectionsHomeComponent implements OnInit {
   data = [
-    { name: "James", age: 24, job: "Designer" },
-    { name: "Alex", age: 28, job: "Frontend Engineer" },
-    { name: "Sara", age: 32, job: "Backend Engineer" }
+    { name: "James", age: 24, job: "Designer", employed: true },
+    { name: "Alex", age: 28, job: "Frontend Engineer", employed: false },
+    { name: "Sara", age: 32, job: "Backend Engineer", employed: true }
   ];
 
   headers = [
+    { key: "employed", label: "Employed" },
     { key: "name", label: "Name" },
     { key: "age", label: "Age" },
     { key: "job", label: "Job" }
