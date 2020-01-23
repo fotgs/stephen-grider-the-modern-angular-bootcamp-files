6d15ec841851158d01fae2ad3e5c7f2d89e77ab7
Annie Giang <agiang@nisum.com>
2020-01-23 10:32:31 -0800

    Reorder routing modules

--- comps/src/app/app.module.ts
+++ comps/src/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ElementsModule } from "./elements/elements.module";
 import { CollectionsModule } from "./collections/collections.module";
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from "./home/home.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, ElementsModule, CollectionsModule],
+  imports: [BrowserModule, ElementsModule, CollectionsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
