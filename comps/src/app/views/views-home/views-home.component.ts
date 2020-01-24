fc8d044386239811db232d791020031a227fb04f
Annie Giang <agiang@nisum.com>
2020-01-24 10:54:55 -0800

    Create ViewsHome

---
+++ comps/src/app/views/views-home/views-home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-view-home",
+  templateUrl: "./views-home.component.html",
+  styleUrls: ["./views-home.component.css"]
+})
+export class ViewsHomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- comps/src/app/views/views.module.ts
+++ comps/src/app/views/views.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { ViewsRoutingModule } from './views-routing.module';
-
+import { ViewsRoutingModule } from "./views-routing.module";
+import { ViewsHomeComponent } from "./views-home/views-home.component";
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ViewsRoutingModule
-  ]
+  declarations: [ViewsHomeComponent],
+  imports: [CommonModule, ViewsRoutingModule]
 })
-export class ViewsModule { }
+export class ViewsModule {}
