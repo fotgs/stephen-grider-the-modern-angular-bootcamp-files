b81f8325d1fc87cf42f269630f829f6fdc527ee7
Annie Giang <agiang@nisum.com>
2020-01-24 13:03:02 -0800

    Render items list

--- comps/src/app/views/views-home/views-home.component.ts
+++ comps/src/app/views/views-home/views-home.component.ts
@@ -17,6 +17,24 @@ export class ViewsHomeComponent implements OnInit {
     { key: "pageViews", label: "Page Views" },
     { key: "users", label: "Users" }
   ];
+
+  items = [
+    {
+      name: "Couch",
+      description:
+        "This is a great couch, the kind that you would want in your house!",
+      imgUrl:
+        "https://s7d3.scene7.com/is/image/ArtVanFurniture/CONDO-BOL-1528BL-7105-SF?$PDPTILEDESKTOP$"
+    },
+    {
+      name: "Dresser",
+      description:
+        "This is a great dresser, the kind that you would want in your house!",
+      imgUrl:
+        "https://www.ikea.com/us/en/images/products/hemnes-chest-of-8-drawers__0627346_PE693299_S5.JPG?f=s"
+    }
+  ];
+
   constructor() {}
 
   ngOnInit() {}
--- comps/src/app/views/views.module.ts
+++ comps/src/app/views/views.module.ts
@@ -5,9 +5,10 @@ import { ViewsRoutingModule } from "./views-routing.module";
 import { ViewsHomeComponent } from "./views-home/views-home.component";
 import { StatisticsComponent } from "./statistics/statistics.component";
 import { SharedModule } from "../shared/shared.module";
+import { ItemsComponent } from './items/items.component';
 
 @NgModule({
-  declarations: [ViewsHomeComponent, StatisticsComponent],
+  declarations: [ViewsHomeComponent, StatisticsComponent, ItemsComponent],
   imports: [CommonModule, ViewsRoutingModule, SharedModule]
 })
 export class ViewsModule {}
