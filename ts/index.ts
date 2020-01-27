f13bb898370419f2af9ea8ff6c6cc474a84e8508
Annie Giang <agiang@nisum.com>
2020-01-27 09:47:19 -0800

    Add typescript for functions

--- ts/index.ts
+++ ts/index.ts
@@ -12,3 +12,12 @@ const isBool: boolean = false;
 const isAny: any = true;
 
 // type inference - only works when a variable is declared AND initialized
+
+// functions
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const joinStrings = (a: string, b: string): string => {
+  return a + b;
+};
