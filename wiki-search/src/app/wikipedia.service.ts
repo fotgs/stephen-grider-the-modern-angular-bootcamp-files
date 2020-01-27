4c553891374e1d7040a7f33a8e860e1262350e4b
Annie Giang <agiang@nisum.com>
2020-01-27 12:44:12 -0800

    Wire up wikipedia service

--- wiki-search/src/app/wikipedia.service.ts
+++ wiki-search/src/app/wikipedia.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class WikipediaService {
+  constructor() {}
 
-  constructor() { }
+  public search(term: string) {
+    // const query = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=${term}`;
+    console.log("wikiiii");
+  }
 }
