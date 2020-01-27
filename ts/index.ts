2c04b49727449ffd2c490b9cd8c889d65e25e8ab
Annie Giang <agiang@nisum.com>
2020-01-27 09:51:21 -0800

    Add typescript for objects

--- ts/index.ts
+++ ts/index.ts
@@ -21,3 +21,14 @@ const add = (a: number, b: number): number => {
 const joinStrings = (a: string, b: string): string => {
   return a + b;
 };
+
+// objects
+const post: { a: number; b: string; c: boolean } = {
+  a: 1,
+  b: "cool",
+  c: false
+};
+
+const printPost = (post): { a: number; b: string; c: boolean } => {
+  return post;
+};
