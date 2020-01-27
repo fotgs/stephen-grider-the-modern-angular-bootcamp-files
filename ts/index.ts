1a6a5a1e78595dda77f5d315dc71bf8fa91b5a32
Annie Giang <agiang@nisum.com>
2020-01-27 10:15:12 -0800

    Add typescript for class

--- ts/index.ts
+++ ts/index.ts
@@ -50,3 +50,27 @@ const printPost2 = (post: Post) => {
 };
 
 // CLASSES
+class Car {
+  //   color = "red";
+  //   year = 2000;
+
+  //   constructor() {
+  //     this.color = color;
+  //     this.year = year;
+  //   }
+
+  color: string;
+  year: number;
+
+  constructor(color: string, year: number) {
+    this.color = color;
+    this.year = year;
+  }
+
+  drive() {
+    console.log("Vroom");
+  }
+}
+
+const myCar = new Car("red", 2000);
+myCar.drive();
