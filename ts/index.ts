c6aca26d2831d5905bc720e586fb620e67131827
Annie Giang <agiang@nisum.com>
2020-01-27 09:36:19 -0800

    Add example of types

--- ts/index.ts
+++ ts/index.ts
@@ -1,2 +1,12 @@
 // type annotation
 const myName: string = "Annie";
+
+const ten: number = 10;
+const negative: number = -100;
+const decimal: number = 10.01;
+
+const isNull: null = null;
+const isUndefined: undefined = undefined;
+
+const isBool: boolean = false;
+const isAny: any = true;
