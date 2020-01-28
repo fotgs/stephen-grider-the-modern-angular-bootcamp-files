57edc3210f882c8f703a1198610fe4b6d164df79
Annie Giang <agiang@nisum.com>
2020-01-28 12:38:21 -0800

    Add more operators and observer

--- rxjs/index.js
+++ rxjs/index.js
@@ -1,5 +1,5 @@
 // JS
-const input = document.querySelector("input"); // observable
+const input = document.querySelector("input"); // observable - emits an addEventListener
 
 input.addEventListener("input", event => {
   // processing pipeline starts here
@@ -27,10 +27,26 @@ container.appendChild(input);
 
 const observable = fromEvent(input, "input") // similar to adding addEventListener
   .pipe(
-    // create the pipe by chaining operator
+    // event flows through this pipeline created by chaining operators
     map(event => event.target.value),
-    map(value => console.log(value))
+    map(value => parse.int(value)),
+    map(value => {
+      if (isNaN(value)) {
+        throw new Error("Enter a number!");
+      }
+      return value;
+    })
   );
+
+// argument passed into subscribe is the observer
+observable.subscribe({
+  next(value) {
+    console.log(`Your value is ${value}`);
+  },
+  error(err) {
+    console.error(err.message);
+  }
+});
 
 // Need to add for this tool, but not in general use.
 observable;
