c12840a5157605980c996a1238ee510d7d9e424b
Annie Giang <agiang@nisum.com>
2020-01-29 12:07:17 -0800

    Setup input validations for expiration input

--- credit-card/src/app/date-form-control.ts
+++ credit-card/src/app/date-form-control.ts
@@ -1,5 +1,31 @@
 import { FormControl } from "@angular/forms";
 
 export class DateFormControl extends FormControl {
-  setValue(value: string, options?: any) {}
+  setValue(value: string, options: any) {
+    if (value.match(/[^0-9|\/]/gi) || value.length > 5) {
+      super.setValue(this.value, {
+        ...options,
+        emitModelToViewChange: true
+      });
+      return;
+    }
+
+    if (value.length === 2 && this.value.length === 3) {
+      super.setValue(value, {
+        ...options,
+        emitModelToViewChange: true
+      });
+      return;
+    }
+
+    if (value.length === 2) {
+      super.setValue(value + "/", {
+        ...options,
+        emitModelToViewChange: true
+      });
+      return;
+    }
+
+    super.setValue(value, { ...options, emitModelToViewChange: true });
+  }
 }
