03a765b85b8381401e983d76f9090b680695be6c
Annie Giang <agiang@nisum.com>
2020-01-29 11:36:08 -0800

    Add validations for new inputs

--- credit-card/src/app/card-form/card-form.component.ts
+++ credit-card/src/app/card-form/card-form.component.ts
@@ -9,9 +9,21 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
     name: new FormControl("", [Validators.required, Validators.minLength(3)]),
-    cardNumber: new FormControl("", [Validators.required]),
-    expiration: new FormControl("", [Validators.required]),
-    securityCode: new FormControl("", [Validators.required])
+    cardNumber: new FormControl("", [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16)
+    ]),
+    expiration: new FormControl("", [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+    ]),
+    securityCode: new FormControl("", [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3),
+      Validators.pattern(/[0-9]/)
+    ])
   });
 
   constructor() {}
