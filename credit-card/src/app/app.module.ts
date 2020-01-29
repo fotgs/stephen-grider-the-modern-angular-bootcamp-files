435534bb0f00db3fca49bcf2bc983bdf03a7f3a8
Annie Giang <agiang@nisum.com>
2020-01-29 12:11:11 -0800

    Import ngx-mask

--- credit-card/src/app/app.module.ts
+++ credit-card/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { CardFormComponent } from './card-form/card-form.component';
-import { InputComponent } from './input/input.component';
+import { CardFormComponent } from "./card-form/card-form.component";
+import { InputComponent } from "./input/input.component";
+import { NgxMaskModule, IConfig } from "ngx-mask";
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 
 @NgModule({
   declarations: [AppComponent, CardFormComponent, InputComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, NgxMaskModule.forRoot(options)],
   providers: [],
   bootstrap: [AppComponent]
 })
